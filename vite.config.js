f4f4c31af5e1bba779db4fc13b38f169b282ecb5
valentina-26 <160797346+valentina-26@users.noreply.github.com>
2024-11-01 12:25:51 -0500

    fix: :wrench: cambio dcript en el indext html

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,14 @@ import { join } from 'path';
 
 export default defineConfig({
   root: join(__dirname, 'client'),
-  base: '/NotesApp/',  // Cambiado de https://... a /NotesApp/
+  base: '/NotesApp/',
   plugins: [vue()],
   build: {
-    outDir: join(__dirname, './client/dist'), // Cambiado para generar dist en la raíz
+    outDir: join(__dirname, 'client/dist'),
     emptyOutDir: true,
     rollupOptions: {
       output: {
-        assetFileNames: (assetInfo) => {
-          let extType = assetInfo.name.split('.').at(1);
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            extType = 'img';
-          }
-          return `assets/${extType}/[name]-[hash][extname]`;
-        },
+        assetFileNames: 'assets/[ext]/[name]-[hash][extname]',
         chunkFileNames: 'assets/js/[name]-[hash].js',
         entryFileNames: 'assets/js/[name]-[hash].js',
       },
